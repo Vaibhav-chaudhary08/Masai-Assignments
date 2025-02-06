51e9fac7a1e466e8475787332358b13ab9f9efc4
Vaibhav Chaudhary <105041233+Vaibhav-chaudhary08@users.noreply.github.com>
2025-02-06 13:04:54 +0530

    Create Question 6.js

---
+++ Question 6.js
@@ -0,0 +1,9 @@
+let temperatures = [25, 28, 22, 27, 30, 29, 26];
+
+console.log("Initial temperatures:", temperatures);
+
+let fourthDayTemperature = temperatures[3];
+
+temperatures[3] = fourthDayTemperature + 2;
+
+console.log("Updated temperatures:", temperatures);
