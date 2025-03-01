33ddfe6cbc6c162bc1ab14e9d6c755856dbd5d9c
Vaibhav Chaudhary <105041233+Vaibhav-chaudhary08@users.noreply.github.com>
2025-03-01 11:59:27 +0530

    Create Error Handling Using AsyncAwait.js

---
+++ Error Handling Using AsyncAwait.js
@@ -0,0 +1,26 @@
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() > 0.5) {
+        resolve("Data fetched successfully!");
+      } else {
+        reject("Failed to fetch data.");
+      }
+    }, 500);
+  });
+}
+
+async function fetchDataHandler() {
+  try {
+    const result = await fetchData();
+    console.log(result);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+}
+
+fetchDataHandler();
+fetchDataHandler();
+fetchDataHandler();
+fetchDataHandler();
+fetchDataHandler();
